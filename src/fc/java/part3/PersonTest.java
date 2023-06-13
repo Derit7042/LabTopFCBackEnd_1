cd412f70ac82a24eda23e1442ba6d4487ecf724b
Junsu An <junsu6752@gmail.com>
2023-06-13 23:43:26 +0900

    2023_06_13_1

---
+++ src/fc/java/part3/PersonTest.java
@@ -0,0 +1,18 @@
+package fc.java.part3;
+
+public class PersonTest {
+    public static void main(String[] args) {
+        // Q. 한 [사람]의 데이터를 저장할 [변수를 선언]하세요.
+        Person p; // 원래는 fc.java.part3.Person p 라고 해야하지만
+                    // 같은 디렉토리 안에 있기 때문에 Person p 만 써도 된다.
+        p = new Person();
+
+        // 모델링 된 Person 클래스를 이용하여 [객체를 메모리에 생성]하세요.
+        // [인스턴스를 만드는 과정]
+        // Person p = new Person(); // new Person(); 이 인스턴스? // p는 인스턴스 변수
+
+
+
+
+    }
+}
