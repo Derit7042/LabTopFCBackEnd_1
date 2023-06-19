bec4b4707770f5b75afb6799a4ed8d1d789d78ee
Junsu An <junsu6752@gmail.com>
2023-06-19 23:10:03 +0900

    2023_06_19_1

--- src/fc/java/part3/CarUtilityTest.java
+++ src/fc/java/part3/CarUtilityTest.java
@@ -34,7 +34,7 @@ public class CarUtilityTest {
 
     }
     // 매개변수로 자동차의 정보를 입력받아서 출력하는 매서드를 정의하세요.
-    /* public static void carPrint(CarDTO car) {
-        System.out.println(car.carSn + "\t " + car.carName + "\t " + car.carPrice + "\t " + car.carOwner + "\t " + car.carYear + "\t " + car.carType);
-    } */
+    // public static void carPrint(CarDTO car) {
+    //    System.out.println(car.carSn + "\t " + car.carName + "\t " + car.carPrice + "\t " + car.carOwner + "\t " + car.carYear + "\t " + car.carType);
+    //}
 }
