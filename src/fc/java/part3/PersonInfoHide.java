42bc4fa051d9c0c4131786881fd6ed6b0e0538a9
Junsu An <junsu6752@gmail.com>
2023-06-26 13:38:16 +0900

    2023_06_26_1

---
+++ src/fc/java/part3/PersonInfoHide.java
@@ -0,0 +1,15 @@
+package fc.java.part3;
+
+import fc.java.model.PersonVO;
+
+public class PersonInfoHide {
+    public static void main(String[] args) {
+        // Q. 한 사람의 [회원] 정보를 저장할 [객체를 생성]하세요.
+        PersonVO vo = new PersonVO();
+        vo.name = "홍길동";
+        vo.age = 5000; // 정보 은닉이 되지 않아서 나이가 5000살로 해도 문제없이 출력된다.
+        vo.phone = "010-1111-1111";
+
+        System.out.println(vo.name + "\t" + vo.age + "\t" + vo.phone);
+    }
+}
