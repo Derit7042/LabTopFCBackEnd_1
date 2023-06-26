64673825a973ada83ae79fbe51e23e6b9b31860a
Junsu An <junsu6752@gmail.com>
2023-06-26 15:00:03 +0900

    2023_06_26_5

---
+++ src/fc/java/model/MovieVO.java
@@ -0,0 +1,87 @@
+package fc.java.model;
+
+public class MovieVO {
+    private String title;
+    private int day;
+    private String major;
+    private String part;
+    private float time;
+    private int level;
+
+    // 우클릭 -> Generate 클릭 -> Constructor -> Select None
+    // 디폴트 생성자
+    public MovieVO() {
+    }
+
+    // 우클릭 -> Generate 클릭 -> Constructor -> 전부 선택하고 OK 클릭
+    public MovieVO(String title, int day, String major, String part, float time, int level) {
+        this.title = title;
+        this.day = day;
+        this.major = major;
+        this.part = part;
+        this.time = time;
+        this.level = level;
+    }
+
+    // 우클릭 -> Generate 클릭 -> Getter and Setter
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public String getPart() {
+        return part;
+    }
+
+    public void setPart(String part) {
+        this.part = part;
+    }
+
+    public float getTime() {
+        return time;
+    }
+
+    public void setTime(float time) {
+        this.time = time;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    // 우클릭 -> Generate 클릭 -> toString()
+    @Override
+    public String toString() {
+        return "MovieVO{" +
+                "title='" + title + '\'' +
+                ", day=" + day +
+                ", major='" + major + '\'' +
+                ", part='" + part + '\'' +
+                ", time=" + time +
+                ", level=" + level +
+                '}';
+    }
+}
