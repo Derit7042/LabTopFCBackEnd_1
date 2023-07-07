84d28da12d5044afcba0b2d4d370567b31fed877
Junsu An <junsu6752@gmail.com>
2023-07-07 17:39:33 +0900

    2023_07_07_1

--- src/fc/java/part4/RempVO.java
+++ src/fc/java/part4/RempVO.java
@@ -4,4 +4,13 @@ public class RempVO extends Employee{
     public RempVO(){
         super();
     }
+    public RempVO(String name, int age, String phone, String empDate, String dept, boolean marriage){
+        // 초기화(자식이 부모의 기억공간에 초기화하는 경우)
+        this.name = name;
+        this.age = age;
+        this.phone = phone;
+        this.empDate = empDate;
+        this.dept = dept;
+        this.marriage = marriage;
+    }
 }
