19ba420dfbac75adb26712eee9ff9e5b6402e141
Junsu An <junsu6752@gmail.com>
2023-07-07 18:14:34 +0900

    2023_07_07_2

--- src/fc/java/part4/RempVO.java
+++ src/fc/java/part4/RempVO.java
@@ -2,15 +2,10 @@ package fc.java.part4;
 // 일반사원(VO)
 public class RempVO extends Employee{
     public RempVO(){
-        super();
+        super(); // 부모의 생성자를 호출
     }
     public RempVO(String name, int age, String phone, String empDate, String dept, boolean marriage){
         // 초기화(자식이 부모의 기억공간에 초기화하는 경우)
-        this.name = name;
-        this.age = age;
-        this.phone = phone;
-        this.empDate = empDate;
-        this.dept = dept;
-        this.marriage = marriage;
+        super(name, age, phone, empDate, dept, marriage); // 부모의 생성자를 호출
     }
 }
