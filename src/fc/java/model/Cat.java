0ab6f47dafcb915e9cabcef93917565db97369ef
Junsu An <junsu6752@gmail.com>
2023-07-14 19:24:55 +0900

    2023_07_14_1

--- src/fc/java/model/Cat.java
+++ src/fc/java/model/Cat.java
@@ -1,7 +1,10 @@
 package fc.java.model;
 
 public class Cat extends Animal{
-        public void night(){
+    public Cat(){
+        super(); // new Animal();
+    }
+    public void night(){
         System.out.println("밤에 눈에서 빛이난다.");
     }
 }
--- src/fc/java/model/Dog.java
+++ src/fc/java/model/Dog.java
@@ -1,5 +1,8 @@
 package fc.java.model;
 
 public class Dog extends Animal{
-
+    public Dog(){
+        super(); // new Animal();
+        int a = 10; // 수행문
+    }
 }
