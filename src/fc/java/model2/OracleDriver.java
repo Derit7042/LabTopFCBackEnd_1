e09929ddd79c69d5e1f5ca844ef20e800dc7d58a
Junsu An <junsu6752@gmail.com>
2023-08-08 14:15:08 +0900

    2023_08_08_3

--- src/fc/java/model2/OracleDriver.java
+++ src/fc/java/model2/OracleDriver.java
@@ -3,6 +3,7 @@ package fc.java.model2;
 public class OracleDriver implements Connection{
     @Override
     public void getConnection(String url, String username, String password) {
+        // 실제적으로 구현이 되어있다.
         System.out.println("url, username, password 정보를 이용하여 oracle DB를 접속 시도 한다.");
     }
 }
