8f04377739b27f03f28cf61890515cd5c1fcc4c8
Junsu An <junsu6752@gmail.com>
2023-08-14 16:12:45 +0900

    2023_08_14_2

---
+++ src/fc/java/model2/FunctionInterfaceTest1.java
@@ -0,0 +1,9 @@
+package fc.java.model2;
+
+public class FunctionInterfaceTest1 {
+    public static void main(String[] args) {
+        MathOperation mo = new MathOperationImpl();
+        int result = mo.operation(10, 20);
+        System.out.println("result = " + result);
+    }
+}
